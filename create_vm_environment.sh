0768a3baf9300cbf7e795620bf595f11f0e15c90
astheeggeggs <duncan.stuart.palmer@gmail.com>
2019-06-25 15:11:40 -0400

    added a test page

--- create_vm_environment.sh
+++ create_vm_environment.sh
@@ -7,7 +7,7 @@ HOME=/home/dpalmer
 INSTALL_DIR=/home/dpalmer/R/
 
 # Install a bunch of packages to get R up and running on the cluster.
-PKGS="bzip2 build-essential zlib1g-dev libbz2-dev liblzma-dev libcurl4-openssl-dev libpcre3-dev gfortran openjdk-8-jdk libxml2-dev libssl-dev"
+PKGS="bzip2 build-essential zlib1g-dev libbz2-dev liblzma-dev libcurl4-openssl-dev libpcre3-dev gfortran openjdk-8-jdk libxml2-dev libssl-dev pandoc libcairo2-dev xorg libpango-1.0-0 xorg openbox"
 for p in $PKGS; do
     sudo apt-get install -y $p
 done
@@ -18,7 +18,7 @@ tar -xvzf R-3.6.0.tar.gz
 rm R-3.6.0.tar.gz
 
 cd R-3.6.0
-./configure --with-readline=no --with-x=no --prefix=$INSTALL_DIR
+./configure --with-readline=no --with-x=yes --prefix=$INSTALL_DIR --enable-memory-profiling --enable-R-shlib --with-blas --with-lapack
 make
 make install
 
@@ -27,10 +27,12 @@ export PATH=$INSTALL_DIR/bin:$PATH
 
 # Install a bunch of packages:
 Rscript -e 'install.packages("xml2", repos="http://cran.rstudio.com")'
+Rscript -e 'install.packages("Cairo", repos="http://cran.rstudio.com")'
 Rscript -e 'install.packages("rvest", repos="http://cran.rstudio.com")'
+Rscript -e 'install.packages("knitr", repos="http://cran.rstudio.com")'
 Rscript -e 'install.packages(c("data.table",
 	"plotly", "crosstalk", "dplyr", "DT", "kableExtra",
-	"formattable", "htmltools"),
+	"formattable", "htmltools", "pander"),
 	repos="http://cran.rstudio.com")'
 
 # Install git.
@@ -40,4 +42,5 @@ cd $HOME
 git clone https://github.com/astheeggeggs/UKBB_ldsc_r2.git
 
 # Move into the scripts folder and create the website.
-
+cd UKBB_ldsc_r2/scripts/
+Rscript render_site.r
