5df5f488b90538280e7dad4ae9f8a5f6b240186a
astheeggeggs <duncan.stuart.palmer@gmail.com>
2019-09-26 12:44:05 +0200

    made repo public, added within sex correlation matrices plots

--- scripts/render_site.r
+++ scripts/render_site.r
@@ -4,21 +4,21 @@ require("rmarkdown")
 setwd("../site")
 rmarkdown::render_site()
 
-# render('UMAP_of_rg.Rmd', output_dir='../docs')
-# render('UMAP_of_rg_final_level.Rmd', output_dir='../docs')
+render('UMAP_of_rg.Rmd', output_dir='../docs')
+render('UMAP_of_rg_final_level.Rmd', output_dir='../docs')
 
-# render('diffusion_map.Rmd', output_dir='../docs')
-# render('diffusion_map_final_level.Rmd', output_dir='../docs')
+render('diffusion_map.Rmd', output_dir='../docs')
+render('diffusion_map_final_level.Rmd', output_dir='../docs')
 
-# render('correlation_plots.Rmd', output_dir='../docs')
-# render('correlation_plots_agglo.Rmd', output_dir='../docs')
-# render('correlation_plots_within_and_across_sexes.Rmd', output_dir='../docs')
+render('correlation_plots.Rmd', output_dir='../docs')
+render('correlation_plots_agglo.Rmd', output_dir='../docs')
+render('correlation_plots_within_and_across_sexes.Rmd', output_dir='../docs')
 
-# render('pca_of_rg.Rmd', output_dir='../docs')
-# render('pca_of_rg_final_level.Rmd', output_dir='../docs')
-# render('pca_pairs.Rmd', output_dir='../docs')
+render('pca_of_rg.Rmd', output_dir='../docs')
+render('pca_of_rg_final_level.Rmd', output_dir='../docs')
+render('pca_pairs.Rmd', output_dir='../docs')
 
-# render('index.Rmd', output_dir='../docs')
+render('index.Rmd', output_dir='../docs')
 
 # yaml for per-pheno sites
 load("../Rdata_outputs/geno_correlation_sig.Rdata")
